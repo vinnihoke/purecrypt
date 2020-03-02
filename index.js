cd8b372147a5717883b574456c21235ef87c4c14
Vinni Hoke <vinni.hoke@gmail.com>
2020-03-02 08:28:16 -0800

    Cleaning out index.js

--- index.js
+++ index.js
@@ -33,32 +33,4 @@ const purecrypt = {
 	}
 }
 
-// const pureCrypt = (method, value) => {
-// 	if (method == "encrypt") {
-// 		let crypted = ''
-// 		const cipher = crypto.createCipheriv(process.env.ALGO, key, iv)
-// 		cipher.on('readable', () => {
-// 			let chunk;
-// 			while (null !== (chunk = cipher.read())) {
-// 				crypted += chunk.toString('hex');
-// 			}
-// 		})
-// 		cipher.write(value);
-// 		cipher.end()
-// 		return crypted
-// 	} else if (method == "decrypt") {
-// 		let decrypted = ''
-// 		const decipher = crypto.createDecipheriv(process.env.ALGO, key, iv)
-// 		decipher.on('readable', () => {
-// 			let chunk;
-// 			while (null !== (chunk = decipher.read())) {
-// 				decrypted += chunk.toString('utf8');
-// 			}
-// 		})
-// 		decipher.write(value, 'hex');
-// 		decipher.end()
-// 		return decrypted
-// 	}
-// }
-
 module.exports = { purecrypt }
